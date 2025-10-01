ec1b3016014425d50a092d56591c39bc83ec7e53
Amjuks <aman.jukaku@gmail.com>
2025-10-01 10:27:47 +0530

    Refactor route paths to remove redundant prefix

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import Terms from './pages/Terms';
 import Header from './components/Header';
+import './style.css';
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="BYS-Legality/privacy" element={<PrivacyPolicy />} />
-        <Route path="BYS-Legality/terms" element={<Terms />} />
-        <Route path="*" element={<Navigate to="BYS-Legality/privacy" />} />
+        <Route path="privacy" element={<PrivacyPolicy />} />
+        <Route path="terms" element={<Terms />} />
+        <Route path="*" element={<Navigate to="privacy" />} />
       </Routes>
     </>
   );
